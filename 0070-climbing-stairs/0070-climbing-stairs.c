513fb57760df804f37563bfa7bc29df224a36515
RITIK KUMAR <ritikabss100@gmail.com>
2026-07-08 13:03:13 +0530

    Time: 0 ms (100%), Space: 8.5 MB (76.33%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.c
@@ -0,0 +1,13 @@
+int climbStairs(int n) {
+    if(n<=2){
+        return n;
+    }
+    int steps[n+1];
+    steps[0]=0;
+    steps[1]=1;
+    steps[2]=2;
+    for(int i=3 ; i<=n ; i++){
+        steps[i] = steps[i-1] + steps[i-2];      // fibonacci sequence
+    }
+    return steps[n];
+}
