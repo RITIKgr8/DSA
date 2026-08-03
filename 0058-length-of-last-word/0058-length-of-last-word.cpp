bf2f0962342542bb4cb7750d55bc308a4537bb7f
RITIK KUMAR <ritikabss100@gmail.com>
2026-08-03 22:19:39 +0530

    Time: 0 ms (100%), Space: 8.8 MB (69.32%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int result = 0;
+        if(s.size() == 0){
+            return 0;
+        }
+        int i= s.size() -1;
+        while(i>=0 && s[i] == ' '){
+            i--;
+        }
+        while(i>=0 && s[i] != ' '){
+            result++;
+            i--;
+        }
+        return result;
+    }
+};
